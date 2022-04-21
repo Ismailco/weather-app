b9fcb911438d9e6fd047d9a51af9d504fd498283
Ismail courr <25227022+Ismailco@users.noreply.github.com>
2022-04-21 15:11:06 +0000

    Fix linters

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,34 +1,28 @@
 const CACHE_NAME = 'version-1';
 const urlsToCache = ['index.html', 'offline.html'];
 const self = this;
-//install a service worker
+// install a service worker
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       console.log('openend cache');
       return cache.addAll(urlsToCache);
-    })
+    }),
   );
 });
-//listen for request
+// listen for request
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then(() => {
-      return fetch(event.request).catch(() => caches.match('offline.html'));
-    })
-  );
+  event.respondWith(caches.match(event.request).then(() => fetch(event.request).catch(() => caches.match('offline.html'))));
 });
-//activate the service worker
+// activate the service worker
 self.addEventListener('activate', (event) => {
   const cacheWhitelist = [];
   cacheWhitelist.push(CACHE_NAME);
   event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (!cacheWhitelist.includes(cacheName)) return caches.delete(cacheName);
-        })
-      );
-    })
+    caches.keys().then((cacheNames) => Promise.all(
+      cacheNames.map((cacheName) => {
+        if (!cacheWhitelist.includes(cacheName)) return caches.delete(cacheName);
+      }),
+    )),
   );
 });
